7d841a611b55355d7f511f2df2b6c87fded6dbb2
Phil Ewels <phil@tallphil.co.uk>
2014-09-22 22:46:08 +0200

    Some limited functionality.. Returns results now\!

--- js/searchCtrl.js
+++ js/searchCtrl.js
@@ -25,14 +25,80 @@ app.controller("searchCtrl", function($scope, $http) {
 				var querykey = s_response.esearchresult.querykey;
 				var retstart = 0;
 				var retmax = 10;
-				var resultsURL = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=sra&retmode=json&query_key='+querykey+'&WebEnv='+webenv+'"&retstart='+retstart+'&retmax='+retmax;
+				var resultsURL = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=sra&retmode=json&query_key='+querykey+'&WebEnv='+webenv+'&retstart='+retstart+'&retmax='+retmax;
 				$scope.summaryURL = resultsURL;
 				$http.get(resultsURL).success(function(response) {
 					$scope.loadingSpinner = 0;
-					console.log(response);
-					$scope.results = response;
+					console.log(response.result);
+					// Parse the stupid embedded XML
+					angular.forEach(response.result, function(value, key) {
+						// Exp XML
+						expXMLraw = $('<div/>').html(value.expxml).text();
+						parser = new DOMParser();
+						expXML = parser.parseFromString(expXMLraw,"text/xml");
+						expJSON = xmlToJson(expXML);
+						if(expJSON.Summary){
+							exp = expJSON.Summary;
+							value.expxml = exp;
+						}
+						
+						// Runs
+						runsXMLraw = $('<div/>').html(value.runs).text();
+						parser = new DOMParser();
+						runsXML = parser.parseFromString(runsXMLraw,"text/xml");
+						runsJSON = xmlToJson(runsXML);
+						if(runsJSON.Run){
+							runs = runsJSON.Run.attributes;
+							value.runs = runs;
+						}
+					});
+					// Remove useless uids object
+					delete response.result.uids;
+					console.log(response.result);
+					$scope.results = response.result;
 				});
 			});
 		}
 		
 });
+
+
+// Stolen frmo http://davidwalsh.name/convert-xml-json
+// Changes XML to JSON
+function xmlToJson(xml) {
+	
+	// Create the return object
+	var obj = {};
+
+	if (xml.nodeType == 1) { // element
+		// do attributes
+		if (xml.attributes.length > 0) {
+		obj["attributes"] = {};
+			for (var j = 0; j < xml.attributes.length; j++) {
+				var attribute = xml.attributes.item(j);
+				obj["attributes"][attribute.nodeName] = attribute.value;
+			}
+		}
+	} else if (xml.nodeType == 3) { // text
+		obj = xml.nodeValue;
+	}
+
+	// do children
+	if (xml.hasChildNodes()) {
+		for(var i = 0; i < xml.childNodes.length; i++) {
+			var item = xml.childNodes.item(i);
+			var nodeName = item.nodeName;
+			if (typeof(obj[nodeName]) == "undefined") {
+				obj[nodeName] = xmlToJson(item);
+			} else {
+				if (typeof(obj[nodeName].push) == "undefined") {
+					var old = obj[nodeName];
+					obj[nodeName] = [];
+					obj[nodeName].push(old);
+				}
+				obj[nodeName].push(xmlToJson(item));
+			}
+		}
+	}
+	return obj;
+};
